411075b813d61b9856cd67ea5841882c4cab89e0
DouglasOttoSf <douglas.davila@objectedge.com>
2023-05-07 01:30:50 -0300

    Adding 'favorites' button to the DetalhesFilmeActivity layout

--- app/src/main/java/com/uniritter/to100ideia/ui/detalhesfilme/DetalhesFilmeActivity.java
+++ app/src/main/java/com/uniritter/to100ideia/ui/detalhesfilme/DetalhesFilmeActivity.java
@@ -1,9 +1,12 @@
 package com.uniritter.to100ideia.ui.detalhesfilme;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.emoji2.text.EmojiCompat;
+
 import com.squareup.picasso.Picasso;
 import com.uniritter.to100ideia.data.model.Filme;
 import com.unirriter.api_filmes.R;
@@ -16,11 +19,11 @@ public class DetalhesFilmeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_filme);
+        setBtn();
 
         TextView textTituloFilme = findViewById(R.id.text_titulo_filme);
         TextView textDescricaoFilme = findViewById(R.id.text_descricao_filme);
         ImageView imagePosterFilme = findViewById(R.id.image_poster_filme);
-
 
         final Filme filme = (Filme) getIntent().getSerializableExtra(EXTRA_FILME);
         textTituloFilme.setText(filme.getTitulo());
@@ -29,5 +32,10 @@ public class DetalhesFilmeActivity extends AppCompatActivity {
         Picasso.get()
                 .load("https://image.tmdb.org/t/p/"+ resolution + "/" + filme.getCaminhoPoster())
                 .into(imagePosterFilme);
+    }
+
+    public void setBtn () {
+        Button addFavoritos = findViewById(R.id.add_favoritos);
+        addFavoritos.setText(EmojiCompat.get().process("\u2b50")+" Adicionar aos Favoritos");
     }
 }
